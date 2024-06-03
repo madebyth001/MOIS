15dcdc7af34fad05f717aff5c739dde1264cb384
madebyth <madebyth13@gmail.com>
2024-06-03 19:16:15 +0300

    main upd

--- cw/__main__.py
+++ cw/__main__.py
@@ -7,23 +7,19 @@ from cw.pretrained_model_provider import PretrainedModelProvider
 _TUNED_MODEL_PATH = Path('tuned_model.pt')
 
 
-def train():
+def train(n):
     data_path = '/Users/madebyth/PycharmProjects/cw2-master/dataset/dataset.yml'
 
     model = PretrainedModelProvider().get_pretrained()
-    model.train(data=data_path, epochs=10, batch=4)
+    model.train(data=data_path, epochs=n, batch=4)
 
 
-def val():
+def val(img: Path | str):
     model = YOLO('/Users/madebyth/PycharmProjects/cw2-master/cw/runs/detect/train2/weights/best.pt')
-    img = Path('/Users/madebyth/Desktop/test1.jpeg')
     results = model.predict(img)
     for result in results:
-
-        print(result.path)
-        d = result.plot()
         result.show()
 
 
 if __name__ == '__main__':
-    val()
+    train(10)
